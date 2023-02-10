051b7435215ba9e8cbb4a0ecfe011272584ba74c
daovantai <daotai2309@gmail.com>
2023-02-10 13:13:03 +0700

    test branch tda_test 10/2/2023

--- DemoTDA/src/main/java/com/example/demotda/test/test.java
+++ DemoTDA/src/main/java/com/example/demotda/test/test.java
@@ -1,2 +1,7 @@
-package com.example.demotda.test;public class test {
+package com.example.demotda.test;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
 }
