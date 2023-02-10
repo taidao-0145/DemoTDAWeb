8c76d85efbecd394f16b9bb136d4e638d757a34d
daovantai <daotai2309@gmail.com>
2023-02-10 14:15:23 +0700

    ok

--- DemoTDA/src/main/java/com/example/demotda/test/test.java
+++ DemoTDA/src/main/java/com/example/demotda/test/test.java
@@ -1,2 +1,6 @@
-package com.example.demotda.test;public class test {
+package com.example.demotda.test;
+public class test {
+    public static void main(String[] args) {
+        System.out.println("tb");
+    }
 }
